a6f448736295834aa71de38a01624ed7588d3299
lucas <lucas.lvn97439@gmail.com>
2024-06-13 13:48:55 +0500

    catch 404 error

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import mainRouter from "./router";
 import { initializeDatabase } from "./models";
 
@@ -8,6 +8,10 @@ const port = 3000;
 app.use(express.json());
 
 app.use("/api", mainRouter);
+
+app.use("*", (req: Request, res: Response) => {
+  return "error 404 - bingo reunion";
+});
 
 initializeDatabase().then(() => {
   app.listen(port, () => {
