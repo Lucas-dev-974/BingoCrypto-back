171ffd9f2e6fa62c7a0f3add75ddaf0fa5a14f65
lucas <lucas.lvn97439@gmail.com>
2024-06-13 12:22:26 +0500

    updgrade database connexion

--- src/models/index.ts
+++ src/models/index.ts
@@ -2,6 +2,7 @@ require("dotenv").config();
 
 import fs from "fs";
 import path from "path";
+import { Dialect } from "sequelize";
 import { Sequelize } from "sequelize-typescript";
 
 const basename = path.basename(__filename);
@@ -11,13 +12,17 @@ const db: any = {};
 const database = process.env.DATABASE || "database";
 const userDB = process.env.USER_DB || "user_db";
 const passwordDB = process.env.PASSWORD_DB || "passord_db";
+const hostDB = process.env.HOST_DB || "localhost";
+const dialectDB = process.env.DIABLECT_DB || "postres";
 
 console.log("database:", database, userDB, passwordDB);
 
 export const initializeDatabase = async (): Promise<typeof db> => {
   let sequelize: Sequelize = new Sequelize(database, userDB, passwordDB, {
-    dialect: "postgres",
+    dialect: dialectDB as Dialect,
+    host: hostDB,
   });
+
   const models = fs
     .readdirSync(__dirname)
     .filter((file) => {
