5714fe49507be8585af9fe8015ca75c88e85fa67
lucas <lucas.lvn97439@gmail.com>
2024-07-18 14:55:17 +0500

    wip socket io

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,32 @@
 import express, { Request, Response } from "express";
-import mainRouter from "./router";
-// import { initializeDatabase } from "./models";
 import AuthMiddleware from "./middleware/jwt";
+import mainRouter from "./router";
+import cors from "cors";
+import { Server as SocketIOServer, type Socket } from "socket.io";
 
-const app = express();
-const port = 3000;
+import { createServer } from "http";
 
+const app: express.Application = express();
+const httpServer = createServer(app);
+
+const io = new SocketIOServer(httpServer);
+
+io.listen(httpServer);
+
+// Set up connection event for socket.io
+io.on("connection", (socket: Socket) => {
+  console.log("a user connected");
+
+  // Set up message event handler
+  socket.on("open", (message: any) => {
+    console.log(message);
+  });
+});
+
+const port = 3001;
 app.use(express.json());
+app.use(cors());
+
 app.use(AuthMiddleware.authenticateToken);
 
 app.use("/api", mainRouter);
@@ -19,6 +39,8 @@ app.use((req: Request, res: Response) => {
   return res.status(404).json("error 404 - bingo reunion");
 });
 
-app.listen(port, () => {
+const rooms = new Map();
+
+httpServer.listen(port, () => {
   console.log(`Express is listening at http://localhost:${port}`);
 });
