2508653f941091c0b735392aa78c7fd348d170ac
lucas <lucas.lvn97439@gmail.com>
2024-09-25 09:12:39 +0500

    refactor

--- src/app.ts
+++ src/app.ts
@@ -2,45 +2,39 @@ import express, { Request, Response } from "express";
 import AuthMiddleware from "./middleware/jwt";
 import mainRouter from "./router";
 import cors from "cors";
-import { Server as SocketIOServer, type Socket } from "socket.io";
+import { Server } from "socket.io";
+import { createServer } from "node:http";
 
-import { createServer } from "http";
-
-const app: express.Application = express();
-const httpServer = createServer(app);
-
-const io = new SocketIOServer(httpServer);
-
-io.listen(httpServer);
-
-// Set up connection event for socket.io
-io.on("connection", (socket: Socket) => {
-  console.log("a user connected");
-
-  // Set up message event handler
-  socket.on("open", (message: any) => {
-    console.log(message);
-  });
-});
+const app = express();
 
 const port = 3001;
+const server = createServer(app);
+
+// Socket setup
+const io = new Server(3002, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected: ", socket.id);
+
+  socket.on("disconnect", () => console.log("user disconnected"));
+  socket.on("join-room", () => {});
+  socket.on("leave-room", () => {});
+});
+
 app.use(express.json());
 app.use(cors());
 
 app.use(AuthMiddleware.authenticateToken);
-
 app.use("/api", mainRouter);
 
 app.get("*", (req: Request, res: Response) => {
   return res.status(404).json("error 404 - bingo reunion");
 });
 
-app.use((req: Request, res: Response) => {
-  return res.status(404).json("error 404 - bingo reunion");
-});
-
-const rooms = new Map();
-
-httpServer.listen(port, () => {
+server.listen(port, () => {
   console.log(`Express is listening at http://localhost:${port}`);
 });
